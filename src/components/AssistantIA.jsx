fb339f7fcd9f298fcffb15a2ad5c7d75dc0d3a29
Jaime Jimenez Nieto <jaime.jimeneznieto@telefonica.com>
2025-11-28 09:47:16 +0100

    feat: initial commit from jaime's branch

--- src/components/AssistantIA.jsx
+++ src/components/AssistantIA.jsx
@@ -1,21 +1,40 @@
-import { Container, Box, Typography, Paper, InputBase, ButtonGroup, Button} from "@mui/material";
-import ChipsGroup from "./ChipsGroup";
-import { MdClose } from "react-icons/md";
-import { FaPlus } from "react-icons/fa6";
-import '../styles/AssistantIA.css';
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import { MdClose } from "react-icons/md";
 
+import EditSquareIcon from '@mui/icons-material/EditSquare';
+import FolderCopyIcon from '@mui/icons-material/FolderCopy';
+import AddIcon from '@mui/icons-material/Add';
+
+import MicIcon from '@mui/icons-material/Mic';
+
+import { HiSparkles } from "react-icons/hi";
+
+import { Box, Typography, Paper, InputBase, ButtonGroup, Button, IconButton} from "@mui/material";
+
+import ChipsGroup from "./ChipsGroup";
+
+import '../styles/AssistantIA.css';
 
 const menuItems = [
-    { text: 'Valor Ganado' },
-    { text: 'Planificación' },
-    { text: 'Riesgos' },
-    { text: 'Reuniones' },
+  { text: 'Valor Ganado' },
+  { text: 'Planificación' },
+  { text: 'Riesgos' },
+  { text: 'Reuniones' },
 ];
 
 
-export default function AssistantIA() {
+export default function AssistantIA({ isOpen, onToggle }) {
   const [value, setValue] = useState("");
+  const [isClosing, setIsClosing] = useState(false);
+  
+  const handleClose = () => {
+    setIsClosing(true);
+    setTimeout(() => {
+      setIsClosing(false);
+      onToggle();
+    }, 300); // Duración de la animación
+  };
   
   const handleChange = (e) => {
     const text = e.target.value;
@@ -29,46 +48,76 @@ export default function AssistantIA() {
     setValue(text);
   };
   return (
-    <Container container>
+    <>
+      <Link href="#" underline="none" className="ia-link" onClick={handleClose} style={{ cursor: 'pointer' }}>
+        <HiSparkles />
+        Asistente IA
+      </Link>
+      { isOpen && (
+        <Box className={`ia-panel ${isClosing ? 'ia-panel-closing' : ''}`}>
+          <Box className="ia-content-1">
+            <Typography className="ia-header-text" variant="body1" fontWeight="bold">
+              <HiSparkles /> 
+              Asistente IA
+            </Typography>
+            <Box className="ia-options">
+                <Box>
+                  <IconButton>
+                    <EditSquareIcon />
+                  </IconButton>
+                  <IconButton className="ia-option-button">
+                    <FolderCopyIcon />
+                  </IconButton>
+                </Box>
+                
+              <IconButton onClick={handleClose} style={{ cursor: 'pointer' }}>
+                <MdClose />
+              </IconButton>
 
-      <Box className="ia-content-1">
-        <Typography variant="body1" fontWeight="bold"> Asistente IA</Typography>
-        <MdClose/>
-      </Box>
+            </Box>
+          </Box>
+          <Box className="ia-content-2">
 
-      <Box className="ia-content-2" >
-        
-        <ChipsGroup menuItems={menuItems}/>
+            <ChipsGroup menuItems={menuItems} />
 
-        <Typography variant="h4">
-          Hola, soy VigIA ¿cómo te puedo ayudar hoy?
-        </Typography>
+            <Typography variant="h4" className="black">
+              Hola, soy VigIA ¿cómo te puedo ayudar hoy?
+            </Typography>
 
-        <Paper elevation={3} className="paper">
-          <InputBase
-            fullWidth
-            value={value}
-            onChange={handleChange}
-            multiline
-            placeholder="Escribe tu mensaje"
-            sx={{ fontSize: 16 }}
-          />
-          
-        </Paper>
+            <Paper elevation={3} className="paper">
+              <InputBase
+                fullWidth
+                value={value}
+                onChange={handleChange}
+                multiline
+                placeholder="Escribe tu mensaje"
+                sx={{ fontSize: 16 }} />
 
-        
-        <ButtonGroup orientation="vertical" aria-label="Vertical button group" className="buttonGroup">
-          <Button variant="outlined" fullWidth>
-            Dame los aspectos más relevantes que debo tener en cuenta hoy
-          </Button>
-          <Button variant="outlined" fullWidth>
-            Informame de las reuniones más importantes de hoy
-          </Button>
-          <Button variant="outlined" fullWidth>
-            Dame un resumen del proyecto
-          </Button>
-        </ButtonGroup>
-      </Box>
-    </Container>
+              <Box display="flex" justifyContent="space-between" alignItems="center" mt={1}>
+                <IconButton>
+                  <AddIcon />
+                </IconButton>
+                <IconButton>
+                  <MicIcon />
+                </IconButton>
+              </Box>
+            </Paper>
+
+
+            <ButtonGroup orientation="vertical" aria-label="Vertical button group" className="buttonGroup">
+              <Button variant="outlined" fullWidth>
+                Dame los aspectos más relevantes que debo tener en cuenta hoy
+              </Button>
+              <Button variant="outlined" fullWidth>
+                Informame de las reuniones más importantes de hoy
+              </Button>
+              <Button variant="outlined" fullWidth>
+                Dame un resumen del proyecto
+              </Button>
+            </ButtonGroup>
+          </Box>
+        </Box>
+      )}
+    </>
   );
 }
